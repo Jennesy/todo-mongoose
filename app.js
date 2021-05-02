24bf4696b396b314fca51c6223ef49d1a3ea0b21
Jennesy <jennesy.dai@gmail.com>
2021-05-02 23:17:03 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
-
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
 const port = 3000
+
+// check mongodb
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // set route
 app.get('/', (req, res) => {
