ccc82ee18e7f3d3bccedfb542b707f6ffb6ac629
Jennesy <jennesy.dai@gmail.com>
2021-05-04 16:11:55 +0800

    add handlebars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -13,9 +14,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// set template engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // set route
 app.get('/', (req, res) => {
-  res.send('This will be a todo list web.')
+  res.render('index')
 })
 
 app.listen(port, () => {
