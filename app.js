e431ec4d9801a6d6de4e6f4846b3e355ef30ae27
Jennesy <jennesy.dai@gmail.com>
2021-05-07 15:42:00 +0800

    add update todo function

--- app.js
+++ app.js
@@ -43,11 +43,32 @@ app.post('/todos', (req, res) => {
 })
 
 app.get('/todos/:id', (req, res) => {
-  Todo.findById(req.params.id)
+  return Todo.findById(req.params.id)
     .lean()
     .then(todo => {
       res.render('detail', { todo })
     })
+    .catch(error => console.log(error))
+})
+
+
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
