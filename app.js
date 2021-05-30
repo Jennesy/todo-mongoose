3fa0869bda8916a5a717dc1f24c9a454aa9657b2
Jennesy <jennesy.dai@gmail.com>
2021-05-30 19:39:12 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 
 // set template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
