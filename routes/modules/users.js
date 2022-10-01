deaf61b2983a108c599564af97446d531235eade
Jennesy <jennesy.dai@gmail.com>
2022-10-01 14:22:03 +0800

    feat: Check empty input on login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,63 +6,80 @@ const passport = require('passport')
 const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
-  res.render('login')
+	res.render('login')
 })
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
+router.post(
+	'/login',
+	(req, res, next) => {
+		const { email, password } = req.body
+		const errors = []
+		if (!email || !password) {
+			errors.push({ message: '所有欄位都是必填。' })
+		}
+		if (errors.length) {
+			return res.render('login', {
+				errors,
+			})
+		}
+		next()
+	},
+	passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/users/login',
+	})
+)
 router.get('/register', (req, res) => {
-  res.render('register')
+	res.render('register')
 })
 router.post('/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  const errors = []
-  if (!name || !email || !password || !confirmPassword) {
-    errors.push({ message: '所有欄位都是必填。' })
-  }
-  if (password !== confirmPassword) {
-    errors.push({ message: '密碼與確認密碼不相符！' })
-  }
-  if (errors.length) {
-    return res.render('register', {
-      errors,
-      name,
-      email,
-      password,
-      confirmPassword
-    })
-  }
-  User.findOne({ email }).then(user => {
-    if (user) {
-      errors.push({ message: '這個 Email 已經註冊過了。' })
-      return res.render('register', {
-        errors,
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => {
-        return bcrypt.hash(password, salt)
-      }).then(hash => {
-        User.create({
-          name,
-          email,
-          password: hash
-        })
-      })
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-  })
+	const { name, email, password, confirmPassword } = req.body
+	const errors = []
+	if (!name || !email || !password || !confirmPassword) {
+		errors.push({ message: '所有欄位都是必填。' })
+	}
+	if (password !== confirmPassword) {
+		errors.push({ message: '密碼與確認密碼不相符！' })
+	}
+	if (errors.length) {
+		return res.render('register', {
+			errors,
+			name,
+			email,
+			password,
+			confirmPassword,
+		})
+	}
+	User.findOne({ email }).then((user) => {
+		if (user) {
+			errors.push({ message: '這個 Email 已經註冊過了。' })
+			return res.render('register', {
+				errors,
+				name,
+				email,
+				password,
+				confirmPassword,
+			})
+		}
+		return bcrypt
+			.genSalt(10)
+			.then((salt) => {
+				return bcrypt.hash(password, salt)
+			})
+			.then((hash) => {
+				User.create({
+					name,
+					email,
+					password: hash,
+				})
+			})
+			.then(() => res.redirect('/'))
+			.catch((err) => console.log(err))
+	})
 })
 router.get('/logout', (req, res) => {
-  req.logout()
-  req.flash('success_msg', '你已經成功登出。')
-  res.redirect('/users/login')
+	req.logout()
+	req.flash('success_msg', '你已經成功登出。')
+	res.redirect('/users/login')
 })
 
 module.exports = router
